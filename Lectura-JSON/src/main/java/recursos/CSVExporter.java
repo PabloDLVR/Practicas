a13c9a8df148e0d367b47c888b6dd819654bb670
vaquero-javi <javiervaquerovila@gmail.com>
2025-05-06 11:23:24 +0200

    Update

--- Lectura-JSON/src/main/java/recursos/CSVExporter.java
+++ Lectura-JSON/src/main/java/recursos/CSVExporter.java
@@ -10,15 +10,17 @@ import java.io.PrintWriter;
 public class CSVExporter {
     public static void exportToCSV(String path, JSONArray productosFiltrados) {
         try (PrintWriter writer = new PrintWriter(new File(path))) {
-            writer.println("code,clientName,description,comentario"); // Cabecera ejemplo
+            writer.println("code,clientName,initialDate,total,description,comentario"); // Cabecera ejemplo
 
             for (int i = 0; i < productosFiltrados.length(); i++) {
                 JSONObject producto = productosFiltrados.getJSONObject(i);
                 String code = producto.optString("code", "");
                 String clientName = producto.optString("clientName", "");
+                String initialDate = producto.optString("initialDate", "");
+                String total = producto.optString("total", "");
                 String description = producto.optString("description", "");
                 String comentario = producto.optString("comment", "");
-                writer.printf("%s,%s,%s,%s%n", code,clientName, description, comentario);
+                writer.printf("%s,%s,%s,%s,%s,%s%n", code,clientName,initialDate,total, description, comentario);
             }
 
             System.out.println("✅ CSV exportado correctamente.");
